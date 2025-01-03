862a1e19fbf85e4c2a5b26985ff0532b312df49f
Andreas Adam <andreas.adam@parcellab.com>
2025-01-03 23:25:44 +0100

    chore: config netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwind from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { reactRouterDevTools } from "react-router-devtools";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-	plugins: [reactRouterDevTools(), reactRouter(), tailwind()],
+	plugins: [reactRouterDevTools(), reactRouter(), tailwind(), netlifyPlugin()],
 });
